a7ef4c758288c0aa74a78b71c5aac7c9d84621f3
hoangunity <hoangdm.141196@gmail.com>
2021-02-10 10:00:45 +0700

    JS - Validate Passwords, Store Data

--- script.js
+++ script.js
@@ -5,20 +5,60 @@ const messageContainer = document.querySelector(".message-container");
 const message = document.getElementById("message");
 
 let isValid = false;
+let passwordsMatch = false;
 
 function validateForm() {
   // Using Constraint API
   isValid = form.checkValidity();
   // Style main message for an error
-  message.textContent = "Please fill out all fields.";
-  message.style.color = "red";
-  messageContainer.style.borderColor = "red";
+  if (!isValid) {
+    message.textContent = "Please fill out all fields.";
+    message.style.color = "red";
+    messageContainer.style.borderColor = "red";
+    return;
+  }
+  // Check to see if passwords match
+  if (password1El.value === password2El.value) {
+    passwordsMatch = true;
+    password1El.style.borderColor = "green";
+    password2El.style.borderColor = "green";
+  } else {
+    passwordsMatch = false;
+    message.textContent = "Make sure password match.";
+    message.style.color = "red";
+    messageContainer.style.borderColor = "red";
+    password1El.style.borderColor = "red";
+    password2El.style.borderColor = "red";
+    return;
+  }
+  // If Form is valid and Passwords match
+  if (isValid && passwordsMatch) {
+    message.textContent = "Successfully Registered!";
+    message.style.color = "green";
+    messageContainer.style.borderColor = "green";
+  }
+}
+
+function storeFormData() {
+  const user = {
+    name: form.name.value,
+    phone: form.phone.value,
+    email: form.email.value,
+    website: form.website.value,
+    password: form.password.value,
+  };
+  // Do something with user data
+  console.log(user);
 }
 
 function processFormData(event) {
   event.preventDefault();
   // Validate Form
   validateForm();
+  // Submit Data if valid
+  if (isValid && passwordsMatch) {
+    storeFormData();
+  }
 }
 
 // Event Listener
